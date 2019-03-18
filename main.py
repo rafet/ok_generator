b86467cd409e9dc617902a4dd3dd3ac582dc2496
rafettopcu <44340673+rafettopcu@users.noreply.github.com>
2019-03-18 16:13:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
+import random
 from flask import Flask
-arr = Flask(__name__)
-@app.root('/')
+app = Flask(__name__)
+@app.route('/')
 def generate():
-    sessiz = list("llllkmttnptp")
+    sessiz = list("llllkttnptp")
     sesli = list("ioaooaooii")
     s = "oki"
-    rand = random.randint(1,3)
+    rand = random.randint(2,4)
     for i in range(rand):
         r2 = random.randint(1,100)
         if r2 % 2 == 0:
-            a = random.choice(sessiz) +random.choice(sessiz)
+            a = random.choice(sessiz) + random.choice(sessiz)
         else:
             a = random.choice(sessiz)
+            
         if a[-1] == 'l':
-            b='o'
+            b = 'o'
         elif a[-1] == 't':
             b = 'i'
         else:
             b = random.choice(sesli)
-        s+=a+b
+        s += a + b
     return s
 
 
-if name == '__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0')
